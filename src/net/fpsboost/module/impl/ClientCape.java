e16290ee96988fbc7a8fb4e6f15d1deefba9c53e
DreamDev <3362321322@qq.com>
2024-11-26 22:26:02 +0800

    增加了对用户输入披风网址的非空校验。

--- src/net/fpsboost/module/impl/ClientCape.java
+++ src/net/fpsboost/module/impl/ClientCape.java
@@ -16,7 +16,9 @@ public class ClientCape extends Module {
 
     @Override
     public void onEnable() {
-        CapeUtil.setCape(JOptionPane.showInputDialog("输入你的披风网址(包括https://)"));
+        String cape = JOptionPane.showInputDialog("输入你的披风网址(包括https://):");
+        if (cape == null)return;
+        CapeUtil.setCape(cape);
         enable = false;
         super.onEnable();
     }
