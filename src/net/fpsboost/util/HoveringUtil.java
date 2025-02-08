4d26679ea9d5fd9be275c19c72d4faf8fb23a1bf
LangYa466 <langya466@gmail.com>
2025-02-08 08:02:32 +0800

    使用gradle包管理器

--- src/net/fpsboost/util/HoveringUtil.java
+++
@@ -1,7 +0,0 @@
-package net.fpsboost.util;
-
-public class HoveringUtil {
-    public static boolean isHovering(float x, float y, float width, float height, int mouseX, int mouseY) {
-        return mouseX >= x && mouseY >= y && mouseX < x + width && mouseY < y + height;
-    }
-}
--- src/net/minecraft/client/gui/controls/ISort.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.client.gui.controls;
-
-import net.minecraft.client.gui.GuiListExtended;
-
-import java.util.List;
-
-public interface ISort {
-    void sort(List<GuiListExtended.IGuiListEntry> entries);
-}
