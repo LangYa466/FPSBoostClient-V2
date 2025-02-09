91c30a952e9e54a3c08f9ffd35d831ca6aeec3ab
LangYa466 <langya466@gmail.com>
2025-02-09 13:49:59 +0800

    fix: 更新版本号为2.14

--- src/main/java/net/fpsboost/Client.java
+++ src/main/java/net/fpsboost/Client.java
@@ -29,7 +29,7 @@ import java.util.Objects;
  */
 public class Client implements Wrapper {
     public static final String name = "FPSBoost-V2";
-    public static final String version = "2.13";
+    public static final String version = "2.14";
     public static boolean isOldVersion;
     public static boolean isDev = false;
     public static final String web = "https://api.fpsboost.cn:444/";
