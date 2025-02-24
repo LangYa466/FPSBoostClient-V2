b6a4458486d946076c95275e04b6eef2658b3fcf
LangYa466 <langya466@gmail.com>
2025-02-24 15:03:49 +0800

    feat(core): 添加资源包搜索功能及优化代码格式

--- src/main/java/net/minecraft/client/gui/GuiScreenResourcePacks.java
+++ src/main/java/net/minecraft/client/gui/GuiScreenResourcePacks.java
@@ -1,6 +1,7 @@
 package net.minecraft.client.gui;
 
 import com.google.common.collect.Lists;
+import lombok.Getter;
 import net.minecraft.client.resources.*;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Util;
@@ -14,14 +15,25 @@ import java.net.URI;
 import java.util.Collections;
 import java.util.List;
 
+// 新增搜索框所需的 import
+import net.minecraft.client.gui.GuiTextField;
+
 public class GuiScreenResourcePacks extends GuiScreen {
     private static final Logger logger = LogManager.getLogger();
     private final GuiScreen parentScreen;
+    @Getter
     private List<ResourcePackListEntry> availableResourcePacks;
+    @Getter
     private List<ResourcePackListEntry> selectedResourcePacks;
+    // 保存所有原始的可用材质包列表，用于搜索过滤
+    private List<ResourcePackListEntry> allAvailableResourcePacks;
+
     private GuiResourcePackAvailable availableResourcePacksList;
     private GuiResourcePackSelected selectedResourcePacksList;
     private boolean changed = false;
+
+    // 搜索框控件
+    private GuiTextField searchBox;
 
     public GuiScreenResourcePacks(GuiScreen parentScreenIn) {
         this.parentScreen = parentScreenIn;
@@ -30,6 +42,11 @@ public class GuiScreenResourcePacks extends GuiScreen {
     public void initGui() {
         this.buttonList.add(new GuiOptionButton(2, this.width / 2 - 154, this.height - 48, I18n.format("resourcePack.openFolder")));
         this.buttonList.add(new GuiOptionButton(1, this.width / 2 + 4, this.height - 48, I18n.format("gui.done")));
+
+        // 初始化搜索框
+        this.searchBox = new GuiTextField(114514, this.fontRendererObj, this.width / 2 - 204, this.height - 73, 200, 20);
+        this.searchBox.setMaxStringLength(50);
+        this.searchBox.setText("");
 
         if (!this.changed) {
             this.availableResourcePacks = Lists.newArrayList();
@@ -43,6 +60,9 @@ public class GuiScreenResourcePacks extends GuiScreen {
                 this.availableResourcePacks.add(new ResourcePackListEntryFound(this, resourcepackrepository$entry));
             }
 
+            // 保存原始列表 用于搜索过滤
+            this.allAvailableResourcePacks = Lists.newArrayList(this.availableResourcePacks);
+
             for (ResourcePackRepository.Entry resourcepackrepository$entry1 : Lists.reverse(resourcepackrepository.getRepositoryEntries())) {
                 this.selectedResourcePacks.add(new ResourcePackListEntryFound(this, resourcepackrepository$entry1));
             }
@@ -50,7 +70,7 @@ public class GuiScreenResourcePacks extends GuiScreen {
             this.selectedResourcePacks.add(new ResourcePackListEntryDefault(this));
         }
 
-        this.availableResourcePacksList = new GuiResourcePackAvailable(this.mc, 200, this.height, this.availableResourcePacks);
+        this.availableResourcePacksList = new GuiResourcePackAvailable(this.mc, 200, this.height - 30, this.availableResourcePacks);
         this.availableResourcePacksList.setSlotXBoundsFromLeft(this.width / 2 - 4 - 200);
         this.availableResourcePacksList.registerScrollButtons(7, 8);
         this.selectedResourcePacksList = new GuiResourcePackSelected(this.mc, 200, this.height, this.selectedResourcePacks);
@@ -64,20 +84,33 @@ public class GuiScreenResourcePacks extends GuiScreen {
         this.availableResourcePacksList.handleMouseInput();
     }
 
+    @Override
+    public void updateScreen() {
+        this.searchBox.updateCursorCounter();
+    }
+
+    @Override
+    protected void keyTyped(char typedChar, int keyCode) throws IOException {
+        super.keyTyped(typedChar, keyCode);
+        this.searchBox.textboxKeyTyped(typedChar, keyCode);
+        // 每次键入后重新过滤列表
+        filterAvailableResourcePacks();
+    }
+
+    @Override
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+        this.searchBox.mouseClicked(mouseX, mouseY, mouseButton);
+        this.availableResourcePacksList.mouseClicked(mouseX, mouseY, mouseButton);
+        this.selectedResourcePacksList.mouseClicked(mouseX, mouseY, mouseButton);
+    }
+
     public boolean hasResourcePackEntry(ResourcePackListEntry p_146961_1_) {
         return this.selectedResourcePacks.contains(p_146961_1_);
     }
 
     public List<ResourcePackListEntry> getListContaining(ResourcePackListEntry p_146962_1_) {
         return this.hasResourcePackEntry(p_146962_1_) ? this.selectedResourcePacks : this.availableResourcePacks;
-    }
-
-    public List<ResourcePackListEntry> getAvailableResourcePacks() {
-        return this.availableResourcePacks;
-    }
-
-    public List<ResourcePackListEntry> getSelectedResourcePacks() {
-        return this.selectedResourcePacks;
     }
 
     protected ResourceLocation actionPerformed(GuiButton button) throws IOException {
@@ -153,26 +186,40 @@ public class GuiScreenResourcePacks extends GuiScreen {
         return null;
     }
 
-    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
-        super.mouseClicked(mouseX, mouseY, mouseButton);
-        this.availableResourcePacksList.mouseClicked(mouseX, mouseY, mouseButton);
-        this.selectedResourcePacksList.mouseClicked(mouseX, mouseY, mouseButton);
-    }
-
     protected void mouseReleased(int mouseX, int mouseY, int state) {
         super.mouseReleased(mouseX, mouseY, state);
     }
 
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         this.drawBackground(0);
+        // 绘制搜索框
         this.availableResourcePacksList.drawScreen(mouseX, mouseY, partialTicks);
         this.selectedResourcePacksList.drawScreen(mouseX, mouseY, partialTicks);
         this.drawCenteredString(this.fontRendererObj, I18n.format("resourcePack.title"), this.width / 2, 16, 16777215);
         this.drawCenteredString(this.fontRendererObj, I18n.format("resourcePack.folderInfo"), this.width / 2 - 77, this.height - 26, 8421504);
         super.drawScreen(mouseX, mouseY, partialTicks);
+        this.searchBox.drawTextBox();
     }
 
     public void markChanged() {
         this.changed = true;
+    }
+
+    // 根据搜索框内容过滤可用材质包列表
+    private void filterAvailableResourcePacks() {
+        String searchText = this.searchBox.getText().toLowerCase();
+        this.availableResourcePacks.clear();
+        if (searchText.isEmpty()) {
+            this.availableResourcePacks.addAll(this.allAvailableResourcePacks);
+        } else {
+            for (ResourcePackListEntry entry : this.allAvailableResourcePacks) {
+                if (entry instanceof ResourcePackListEntryFound) {
+                    String packName = ((ResourcePackListEntryFound) entry).func_148318_i().getResourcePackName();
+                    if (packName.toLowerCase().contains(searchText)) {
+                        this.availableResourcePacks.add(entry);
+                    }
+                }
+            }
+        }
     }
 }
