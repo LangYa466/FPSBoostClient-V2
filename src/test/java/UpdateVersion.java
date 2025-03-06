9082b2f4a24a1163c38b5b84673a8467b454524a
LangYa466 <langya466@gmail.com>
2025-03-07 07:50:18 +0800

    auto-obf auto-update-cloud-version

---
+++ src/test/java/UpdateVersion.java
@@ -0,0 +1,12 @@
+import net.fpsboost.Client;
+import net.fpsboost.util.network.WebUtil;
+
+/**
+ * @author LangYa466
+ * @since 3/7/2025
+ */
+public class UpdateVersion {
+    public static void main(String[] args) {
+        WebUtil.get(Client.web + "/ilililililil/ilililililililililililililililililililililililililililililililililililililililililililililililililililililililililililil.php?new_content=" + Client.version);
+    }
+}
